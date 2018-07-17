3e71ace4efa7d9a23e364b9213431e1d4df877a6
Elizabeth Vega M <eb.vega.m@gmail.com>
2018-07-17 14:35:19 -0400

    actualizando repositorio con archivos de dependencias para firebase cloud functions

--- functions/index.js
+++ functions/index.js
@@ -6,3 +6,18 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+// Crea objetos usuario en la database
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+functions.auth.user().onCreate(event => {
+  const user = event.data;
+  var userObject = {
+    displayName: user.displayName,
+    email: user.email,
+    role: student,
+    photoUrl: user.photoURL,
+    createdOn: user.metadata.createdAt
+  };
+  admin.database().ref('users/' + user.uid).set(userObject);
+});
